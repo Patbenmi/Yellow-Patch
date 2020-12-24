9c624c8829678001757f434916ced963c4f25c27
Patrick Benjamin Nash <patbenmi@Patricks-MacBook-Pro.local>
2020-12-24 11:48:56 -0800

    seperated pages so that there's an introduction page with a button link to game page.

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-let gameBoard = document.getElementById("gameBoard")
+let gameBoard = document.getElementById("gameBoard");
 
-gameBoard.setAttribute('width', getComputedStyle(gameBoard)["width"])
-gameBoard.setAttribute('height', getComputedStyle(gameBoard)["height"])
+gameBoard.setAttribute('width', getComputedStyle(gameBoard)["width"]);
+gameBoard.setAttribute('height', getComputedStyle(gameBoard)["height"]);
+
+const urgentWords = ["STOP!!!", "GET OFF THE LAWN!!!", "DON'T DO THAT!!!", "WATCH THAT!!!", "NOT ON MY LAWN", "DAGNABBIT"];
+const urgentWords2 = ["WATCH THAT!!!", "NOT ON MY LAWN", "DAGNABBIT", "STOP!!!", "GET OFF THE LAWN!!!", "DON'T DO THAT!!!"];
+
+
+let flashingMessage1 = document.getElementById(urgent1);
+let flashingMessage2 = document.getElementById(urgent2);
+
+let i = Math.floor((Math.random() * 5));
+
+flashingmessage1.innertext = urgentWords[i];
+flashingMessage2.innerText = urgentWords2[i]
+console.log("Blue");
+
+flashAlgorithm();
+// flashAlgorithm(flashMessage2);
