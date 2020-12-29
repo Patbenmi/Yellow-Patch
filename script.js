2363a1cf63ef512fc334e5090b2a98c27720c135
Patrick Benjamin Nash <patbenmi@Patricks-MacBook-Pro.local>
2020-12-29 15:23:46 -0800

    adding game logic to the JavaScript.

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const urgentWords2 = ["WATCH THAT!!!", "NOT ON MY LAWN", "DAGNABBIT", "STOP!!!",
 
 let flashingMessage1 = document.getElementById(urgent1);
 let flashingMessage2 = document.getElementById(urgent2);
+let urineCount = document.getElementById(urine);
+let scoreCount = document.getElementById()
 
 let i = Math.floor((Math.random() * 5));
 
@@ -16,3 +18,32 @@ let i = Math.floor((Math.random() * 5));
 // flashingMessage2.innerText = urgentWords2[i];
 console.log("Blue");
 
+let context = gameBoard.getContext("2d")
+
+let anitaPettigrew = {
+  urine: false,
+  x = 20,
+  y = 20,
+  color: #peach,
+  width: 20,
+  height: 20,
+  render: function () {
+    context.fillStyle = this.color;
+    context.fillRect = (this.x, this.y, this.width, this.height);
+  };
+};
+let pomeranian = {
+  urine: false,
+  x = 20,
+  y = 20,
+  color: #orange,
+  width: 20,
+  height: 20,
+  render: function() {
+    context.fillStyle = this.color;
+    context.fillStyle = (this.x, this.y, this.width, this.height);
+  };
+};
+let gamePlay =() => {
+  context.clearRect(0,0, gameBoard.width, gameBoard.height);
+}
